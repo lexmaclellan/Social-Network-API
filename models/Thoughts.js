69632b78262d8807425e085374ae171dc403914c
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-09-08 01:45:27 -0300

    Outline of thoughts model

---
+++ models/Thoughts.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+
+        },
+        username: {
+
+        },
+        reactions: {
+            
+        }
+    }
+)
