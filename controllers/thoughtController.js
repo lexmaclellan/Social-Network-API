675ba654c457bc538f2fc81562a595f8ffb34afa
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-09-10 03:18:12 -0300

    Added controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,68 @@
+const { Thought, User } = require('../models');
+
+module.export = {
+
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getOneThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+                .select('-__v');
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID.' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with this ID.' });
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with that ID.' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,70 @@
-const { ObjectId } = require('mongoose').Types;
-const { Student, Course } = require('../models');
+const { User } = require('../models');
+
+module.export = {
+
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getOneUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
+            
+                if (!user) {
+                    return res.status(404).json({ message: 'No user with that ID.' });
+                }
+
+                res.json(user);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) { 
+                res.status(404).json({ message: 'No user with this ID.' });
+            }
+
+            //await User.deleteMany({ _id: { $in: user.thoughts }});
+            res.json({ message: 'User and Thoughts deleted.' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                res.status(404).json({ message: 'No user with that ID.' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
