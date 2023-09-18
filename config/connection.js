27ec3dceb498504e0e89f3dd14648400aec107d0
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-09-18 01:15:38 -0300

    Changed to Mongoose connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,4 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB');
 
-const connectionString = process.env.MONGODB_URI;
-
-connect(connectionString);
-
-module.exports = connection;
+module.exports = mongoose.connection;
