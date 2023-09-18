456f995827039a45b413767b34dd389f7f567c5c
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-09-18 02:12:50 -0300

    Added reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,10 +5,14 @@ const {
     getOneThought,
     createThought,
     deleteThought,
-    updateThought
+    updateThought,
+    addReaction,
+    removeReaction
 } = require('../../controllers/thoughtController');
 
-router.route("/").get(getAllThoughts).post(createThought);
-router.route("/:thoughtId").get(getOneThought).put(updateThought).delete(deleteThought);
+router.route('/').get(getAllThoughts).post(createThought);
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
