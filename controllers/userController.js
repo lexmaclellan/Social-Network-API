9b336d47223f2d728dc8293566c2deb477ef3f9a
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-09-18 01:49:58 -0300

    Added functions to add and remove friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,5 +66,41 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
+    },
+
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { new: true }
+            );
+
+            if (!user) {
+                res.status(404).json({ message: 'No user with that ID.' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async removeFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { new: true }
+            );
+
+            if(!user) {
+                res.status(404).json({ message: 'No user with that ID.' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 }
